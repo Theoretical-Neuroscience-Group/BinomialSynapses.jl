2fdb39dc3708dff6d5fed4bc05cadbc1604c7f5c
Simone Carlo Surace <simsurace@gmx.net>
2021-04-16 19:04:23 +0200

    so many typos

--- src/BinomialSynapses.jl
+++ src/BinomialSynapses.jl
@@ -8,14 +8,16 @@ using Statistics: mean
 # data structures and types
 include("types.jl")
 export BinomialModel
+export BinomialState
 
 # filtering part
 include("propagate.jl")
 include("likelihood.jl")
+include("resample.jl")
+export
         likelihood,
         likelihood_resample!,
-        propagate!
-include("resample.jl")
+        propagate!,
         outer_resample!
 
 end#module
