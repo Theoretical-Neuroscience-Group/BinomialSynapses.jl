5830f8fc5bccab5309dd2c3d843d1a6740466f29
Simone Carlo Surace <simsurace@gmx.net>
2021-04-18 22:56:37 +0200

    clean up jittering code

--- src/jitter.jl
+++ src/jitter.jl
@@ -1,160 +1,40 @@
 using CUDA
 
-function kernel_draw_theta!(
-    indices,
-    N_range,
-    p_range,
-    q_range,
-    sigma_range,
-    tau_range,
-    jittering_kernel_width,
-    r
-)
-    M_out, n_parameters = size(indices)
-    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    @inbounds if i <= M_out
+function jitter!(model::BinomialGridModel, width)
+    prob = Float32(1/width)
+    
+    jitter!(model.Nind,     length(model.Nrng),     prob)
+    jitter!(model.pind,     length(model.prng),     prob)
+    jitter!(model.qind,     length(model.qrng),     prob)
+    jitter!(model.sigmaind, length(model.sigmarng), prob)
+    jitter!(model.tauind,   length(model.taurng),   prob)
 
-        # Initializes the indices
-        index_N = indices[i,1]
-        index_p = indices[i,2]
-        index_q = indices[i,3]
-        index_sigma = indices[i,4]
-        index_tau = indices[i,5]
-
-        # Initializes the random flips
-        random_flip_N = 0
-        random_flip_p = 0
-        random_flip_q = 0
-        random_flip_sigma = 0
-        random_flip_tau = 0
-        random_flip_N_edge = 0
-        random_flip_p_edge = 0
-        random_flip_q_edge = 0
-        random_flip_sigma_edge = 0
-        random_flip_tau_edge = 0
-
-        # Draws the random flips
-        if r[i,1] < 1/(jittering_kernel_width-1)
-            random_flip_N_edge = 1
-        end
-        if r[i,2] < 1/(jittering_kernel_width-1)
-            random_flip_p_edge = 1
-        end
-        if r[i,3] < 1/(jittering_kernel_width-1)
-            random_flip_q_edge = 1
-        end
-        if r[i,4] < 1/(jittering_kernel_width-1)
-            random_flip_sigma_edge = 1
-        end
-        if r[i,5] < 1/(jittering_kernel_width-1)
-            random_flip_tau_edge = 1
-        end
-
-        if r[i,1] < 1/jittering_kernel_width
-            random_flip_N = 1
-        elseif r[i,1] > 1-1/jittering_kernel_width
-            random_flip_N = -1
-        end
-
-        if r[i,2] < 1/jittering_kernel_width
-            random_flip_p = 1
-        elseif r[i,2] > 1-1/jittering_kernel_width
-            random_flip_p = -1
-        end
-
-        if r[i,3] < 1/jittering_kernel_width
-            random_flip_q = 1
-        elseif r[i,3] > 1-1/jittering_kernel_width
-            random_flip_q = -1
-        end
-
-        if r[i,4] < 1/jittering_kernel_width
-            random_flip_sigma = 1
-        elseif r[i,4] > 1-1/jittering_kernel_width
-            random_flip_sigma = -1
-        end
-
-        if r[i,5] < 1/jittering_kernel_width
-            random_flip_tau = 1
-        elseif r[i,5] > 1-1/jittering_kernel_width
-            random_flip_tau = -1
-        end
-
-        # Computes the new indices
-        if indices[i,1] == 1
-            index_N = indices[i,1]+random_flip_N_edge
-        elseif indices[i,1] == length(N_range)
-            index_N = indices[i,1]-random_flip_N_edge
-        else
-            index_N = indices[i,1]+random_flip_N
-        end
-        if indices[i,2] == 1
-            index_p = indices[i,2]+random_flip_p_edge
-        elseif indices[i,2] == length(p_range)
-            index_p = indices[i,2]-random_flip_p_edge
-        else
-            index_p = indices[i,2]+random_flip_p
-        end
-        if indices[i,3] == 1
-            index_q = indices[i,3]+random_flip_q_edge
-        elseif indices[i,3] == length(q_range)
-            index_q = indices[i,3]-random_flip_q_edge
-        else
-            index_q = indices[i,3]+random_flip_q
-        end
-        if indices[i,4] == 1
-            index_sigma = indices[i,4]+random_flip_sigma_edge
-        elseif indices[i,4] == length(sigma_range)
-            index_sigma = indices[i,4]-random_flip_sigma_edge
-        else
-            index_sigma = indices[i,4]+random_flip_sigma
-        end
-        if indices[i,5] == 1
-            index_tau = indices[i,5]+random_flip_tau_edge
-        elseif indices[i,5] == length(tau_range)
-            index_tau = indices[i,5]-random_flip_tau_edge
-        else
-            index_tau = indices[i,5]+random_flip_tau
-        end
-
-        indices[i,1] = index_N
-        indices[i,2] = index_p
-        indices[i,3] = index_q
-        indices[i,4] = index_sigma
-        indices[i,5] = index_tau
-    end
-    return nothing
-end
-
-function jitter!(model::BinomialGridModel, jittering_kernel_width)
-    indices = hcat(model.Nind, model.pind, model.qind, model.sigmaind, model.tauind)
-    M_out, n_parameters = size(indices)
-    r       = CUDA.rand(M_out, n_parameters)
-    kernel  = @cuda name="draw_theta" launch=false kernel_draw_theta!(
-        indices,
-        model.Nrng,
-        model.prng,
-        model.qrng,
-        model.sigmarng,
-        model.taurng,
-        jittering_kernel_width,
-        r
-    )
-    config  = launch_configuration(kernel.fun)
-    threads = Base.min(M_out, config.threads, 256)
-    blocks  = cld(M_out, threads)
-    kernel(
-        indices,
-        model.Nrng,
-        model.prng,
-        model.qrng,
-        model.sigmarng,
-        model.taurng,
-        jittering_kernel_width,
-        r;
-        threads=threads,
-        blocks=blocks
-    )
     refresh!(model)
     return model
+end
+
+function jitter!(indices, maxindex, prob)
+    function kernel(indices, range, prob, randstates)
+        i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+        @inbounds if i <= length(indices)
+            r   = GPUArrays.gpu_rand(Float32, CUDA.CuKernelContext(), randstates)
+            idx = indices[i]
+            if r < prob && idx < maxindex
+                idx += 1
+            elseif r > 1-prob && idx > 1
+                idx -= 1
+            end
+            indices[i] = idx
+        end#if
+        return nothing
+    end
+
+    rng = GPUArrays.default_rng(CuArray)
+
+    kernel  = @cuda launch=false kernel(indices, maxindex, prob, rng.state)
+    config  = launch_configuration(kernel.fun)
+    threads = Base.min(length(indices), config.threads, 256)
+    blocks  = cld(length(indices), threads)
+    kernel(indices, maxindex, prob, rng.state; threads=threads, blocks=blocks)
+    return indices
 end
