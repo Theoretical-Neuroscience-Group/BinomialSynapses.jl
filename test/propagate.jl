9c92427501ca8a178db38c0195bdaafc78263f9c
Simone Carlo Surace <simsurace@gmx.net>
2021-04-18 17:37:23 +0200

    increase test robustness

--- test/propagate.jl
+++ test/propagate.jl
@@ -34,10 +34,10 @@
     qs     = CUDA.rand(m_out)
     sigmas = CUDA.rand(m_out)
     taus   = CUDA.rand(m_out)
-    taus[1] = 100
+    taus[1] = 1000
     taus[2] = 0.0001
     taus[3] = 0.0001
-    taus[4] = 100
+    taus[4] = 1000
     model  = BinomialModel(Ns, ps, qs, sigmas, taus);
     ns = 5 .* CUDA.ones(Int, m_out, m_in)
     ks = 4 .* CUDA.ones(Int, m_out, m_in)
@@ -45,7 +45,6 @@
     propagate!(ns, ks, model, dt)
 
     # When tau is much larger than dt, no vesicle is refilled
-    println(ns)
     @test all(ns[1,:] .== 1)
     @test all(ns[4,:] .== 1)
     # When tau is much shorter than dt, all vesicles are refilled
