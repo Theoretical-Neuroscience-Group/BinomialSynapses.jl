40033e58955176f9f5ef3dc71e86dd2204b9f6b1
Simone Carlo Surace <simsurace@gmx.net>
2021-04-18 23:28:13 +0200

    improve performance of outer_resample!
    more than 5x improvement

--- src/resample.jl
+++ src/resample.jl
@@ -1,81 +1,50 @@
-# Same algorithm as in https://tianjun.me/essays/Categorical_Sampling_on_GPU_with_Julia
-function make_alias_table!(w::AbstractVector{T}, wsum::S,
-                           a::AbstractVector{T},
-                           alias::AbstractVector{<:Integer}) where {S, T}
-    n = length(w)
-    length(a) == length(alias) == n ||
-        throw(DimensionMismatch("Inconsistent array lengths."))
-
-    ac = n / wsum
-    for i = 1:n
-        @inbounds a[i] = w[i] * ac
+function kernel_outer_indices!(idx, u, r, randstates)
+    M_out = length(u)
+    usum = 0f0
+    CurMax = 1f0
+    @inbounds for i in 1:M_out
+        usum += u[i]
+        u[i]  = usum
+        # sample descending sequence of sorted random numbers
+        # r[M_in] >= ... >= r[2] >= r[1]
+        # Algorithm by:
+        # Bentley & Saxe, ACM Transactions on Mathematical Software, Vol 6, No 3
+        # September 1980, Pages 359--364
+        mirrori    = M_out - i + 1 # mirrored index i
+        CurMax    *= CUDA.exp(CUDA.log(GPUArrays.gpu_rand(Float32, CUDA.CuKernelContext(), randstates)) / mirrori)
+        r[mirrori] = CurMax
     end
-
-    larges = Vector{Int}(undef, n)
-    smalls = Vector{Int}(undef, n)
-    kl = 0  # actual number of larges
-    ks = 0  # actual number of smalls
-
-    for i = 1:n
-        @inbounds ai = a[i]
-        if ai > 1.0
-            larges[kl+=1] = i  # push to larges
-        elseif ai < 1.0
-            smalls[ks+=1] = i  # push to smalls
+    bindex = 1 # bin index
+    @inbounds for i in 1:M_out
+        # scale random numbers (this is equivalent to normalizing v)
+        rsample = r[i] * usum
+        # checking bindex <= M_in - 1 is redundant since
+        # v[M_in, j] = vsum and
+        while rsample > u[bindex]
+            bindex += 1
         end
+        idx[i] = bindex
     end
-
-    while kl > 0 && ks > 0
-        s = smalls[ks]; ks -= 1  # pop from smalls
-        l = larges[kl]; kl -= 1  # pop from larges
-        @inbounds alias[s] = l
-        @inbounds al = a[l] = (a[l] - 1.0) + a[s]
-        if al > 1.0
-            larges[kl+=1] = l  # push to larges
-        else
-            smalls[ks+=1] = l  # push to smalls
-        end
-    end
-
-    # this loop should be redundant, except for rounding
-    for i = 1:ks
-        @inbounds a[smalls[i]] = 1.0
-    end
-    nothing
+    return nothing
 end
 
-function cu_alias_sample!(a::CuArray{Ta}, wv::AbstractVector{Tw}, x::CuArray{Ta}) where {Tw<:Number, Ta}
-    length(a) == length(wv) || throw(DimensionMismatch("weight vector must have the same length with label vector"))
-    n = length(wv)
-    # create alias table
-    ap = Vector{Tw}(undef, n)
-    alias = Vector{Int64}(undef, n)
-    make_alias_table!(wv, sum(wv), ap, alias)
+function outer_indices(u)
+    M_out       = length(u)
+    idx         = CuArray{Int}(undef, M_out)
+    r           = CuArray{Float32}(undef, M_out)
 
-    # to device
-    alias = CuArray{Int64}(alias)
-    ap = CuArray{Tw}(ap)
+    rng = GPUArrays.default_rng(CuArray)
 
-    function kernel(state, _, (alias, ap, x, a, randstate))
-        r1, r2 = GPUArrays.gpu_rand(Float32, state, randstate), GPUArrays.gpu_rand(Float32, state, randstate)
-        r1 = r1 == 1.0 ? 0.0 : r1
-        r2 = r2 == 1.0 ? 0.0 : r2
-        i = linear_index(state)
-        if i <= length(x)
-            j = floor(Int, r1 * n) + 1
-            @inbounds x[i] = r2 < ap[j] ? a[j] : a[alias[j]]
-        end
-        return
-    end
-    gpu_call(kernel, x, (alias, ap, x, a, GPUArrays.default_rng(typeof(x)).state))
-    x
+    kernel  = @cuda launch=false kernel_outer_indices!(idx, u, r, rng.state)
+    config  = launch_configuration(kernel.fun)
+    threads = Base.min(M_out, config.threads, 256)
+    blocks  = cld(M_out, threads)
+    kernel(idx, u, r, rng.state; threads=threads, blocks=blocks)
+    return idx
 end
 
 function outer_resample!(state::BinomialState, u)
-    M_out = size(state.n, 1)
-    a = CuArray{Int64}(1:M_out)
-    x = CuArray{Int64}(zeros(Int64, M_out))
-    cu_alias_sample!(a,u,x)
-    state.n .= state.n[x,:]
-    state.k .= state.k[x,:]
+    idx = outer_indices(u)
+    state.n .= state.n[idx,:]
+    state.k .= state.k[idx,:]
 end
