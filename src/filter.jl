1e12beb733f89dc6d27edef38fbd8cc07de6d24c
Simone Carlo Surace <simsurace@gmx.net>
2021-04-19 13:25:29 +0200

    add convenience constructor for NestedParticleState

--- src/filter.jl
+++ src/filter.jl
@@ -7,6 +7,18 @@ struct NestedParticleState{T1, T2}
     model::T2
 end
 
+function NestedParticleState(
+    m_out::Int, m_in::Int,
+    my_Nrng, my_prng, my_qrng, my_sigmarng, my_taurng
+)
+    model = BinomialGridModel(m_out, my_Nrng, my_prng, my_qrng, my_sigmarng, my_taurng)
+    n     = repeat(model.N, 1, m_in)
+    k     = CUDA.zeros(Int, m_out, m_in)
+    state = BinomialState(n, k)
+    propagate!(state, model, 0f0)
+    return NestedParticleState(state, model)
+end
+
 function update!(
     filterstate::NestedParticleState,
     observation::BinomialObservation,
