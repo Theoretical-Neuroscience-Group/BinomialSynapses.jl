3b9c0737a44d6238a6238a541111e1582c75f8c8
Simone Carlo Surace <simsurace@gmx.net>
2021-04-19 13:48:54 +0200

    simplify test script

--- test/propagate.jl
+++ test/propagate.jl
@@ -1,56 +1,52 @@
 @testset "propagate!" begin
     m_out  = 1024
     m_in   = 1024
-    Ns     = CuArray(rand(1:128, m_out))
-    ps     = CUDA.rand(m_out)
-    qs     = CUDA.rand(m_out)
-    sigmas = CUDA.rand(m_out)
-    taus   = CUDA.rand(m_out)
-    model  = BinomialModel(Ns, ps, qs, sigmas, taus);
 
-    ns = CuArray(rand(1:128, m_out, m_in))
-    ks = CUDA.zeros(Int, m_out, m_in);
+    state = BinomialState(128, m_out, m_in)
+    model = BinomialModel(128, m_out)
 
     for i in 1:100
-        propagate!(ns, ks, model, 0.1f0)
-        @test minimum(ns) >= 0
-        @test minimum(ks) >= 0
-        @test minimum(ns .- ks) >= 0
-        @test minimum(Ns .- ns) >= 0
+        propagate!(state, model, 0.1f0)
+        @test minimum(state.n) >= 0
+        @test minimum(state.k) >= 0
+        @test minimum(state.n .- state.k) >= 0
+        @test minimum(state.n .- state.n) >= 0
     end
 
     println("")
     println("Benchmarking function propagate!: should take about 4ms")
-    display(@benchmark CUDA.@sync propagate!($ns, $ks, $model, 0.1f0))
+    display(@benchmark CUDA.@sync propagate!($state, $model, 0.1f0))
     println("")
 
     dt     = 0.1
     m_out  = 4
     m_in   = 4
-    Ns     = 10 .* CUDA.ones(Int, m_out)
-    ps     = cu([0f0, 0f0, 1f0, 1f0])
-    qs     = CUDA.rand(m_out)
-    sigmas = CUDA.rand(m_out)
-    taus   = cu([1f3, 1f-4, 1f-4, 1f3])
-    model  = BinomialModel(Ns, ps, qs, sigmas, taus);
-    ns     = 5 .* CUDA.ones(Int, m_out, m_in)
-    ks     = 4 .* CUDA.ones(Int, m_out, m_in)
+    N      = 10 .* CUDA.ones(Int, m_out)
+    p      = cu([0f0, 0f0, 1f0, 1f0])
+    q      = CUDA.rand(m_out)
+    sigma  = CUDA.rand(m_out)
+    tau    = cu([1f3, 1f-4, 1f-4, 1f3])
+    model  = BinomialModel(N, p, q, sigma, tau);
+    n      = 5 .* CUDA.ones(Int, m_out, m_in)
+    k      = 4 .* CUDA.ones(Int, m_out, m_in)
 
-    propagate!(ns, ks, model, dt)
-    ns = Array(ns)
-    ks = Array(ks)
+    state = BinomialState(n, k)
+
+    propagate!(state, model, dt)
+    n = Array(state.n)
+    k = Array(state.k)
 
     # When tau is much larger than dt, no vesicle is refilled
-    @test all(ns[1,:] .== 1)
-    @test all(ns[4,:] .== 1)
+    @test all(n[1,:] .== 1)
+    @test all(n[4,:] .== 1)
     # When tau is much shorter than dt, all vesicles are refilled
-    @test all(ns[2,:] .== 10)
-    @test all(ns[3,:] .== 10)
+    @test all(n[2,:] .== 10)
+    @test all(n[3,:] .== 10)
 
     # When the release probability is 0, no vesicle is released
-    @test all(ks[1,:] .== 0)
-    @test all(ks[2,:] .== 0)
+    @test all(k[1,:] .== 0)
+    @test all(k[2,:] .== 0)
     # When the release probability is 1, all vesicles are released
-    @test all(ks[3,:] .== 10)
-    @test all(ks[4,:] .== 1)
+    @test all(k[3,:] .== 10)
+    @test all(k[4,:] .== 1)
 end
