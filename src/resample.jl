a3898ec325f780cad7e6558dbfab6a54dca5a8aa
Simone Carlo Surace <simsurace@gmx.net>
2021-04-20 02:03:25 +0200

    put outer_resample on CPU
    fixes bug and makes it 3x faster

--- src/resample.jl
+++ src/resample.jl
@@ -1,46 +1,42 @@
-function kernel_outer_indices!(idx, u, r, randstates)
-    M_out = length(u)
-    usum = u[M_out]
-    CurMax = 1f0
+function outer_indices!(u)
+    uu = Array(u)
+    idx = outer_indices!(uu)
+    return cu(idx)
+end
+
+function outer_indices!(u::Vector)
+    M_out  = length(u)
+    usum   = 0f0
+    # overwrite u with cumulative sum
     @inbounds for i in 1:M_out
-        #usum += u[i]
-        #u[i]  = usum
-        # sample descending sequence of sorted random numbers
-        # r[M_in] >= ... >= r[2] >= r[1]
-        # Algorithm by:
-        # Bentley & Saxe, ACM Transactions on Mathematical Software, Vol 6, No 3
-        # September 1980, Pages 359--364
-        mirrori    = M_out - i + 1 # mirrored index i
-        CurMax    *= CUDA.exp(CUDA.log(GPUArrays.gpu_rand(Float32, CUDA.CuKernelContext(), randstates)) / mirrori)
-        r[mirrori] = CurMax
+        usum += u[i]
+        u[i] = usum
     end
-    bindex = 1 # bin index
-    @inbounds for i in 1:M_out
+
+    # shift cumulative sums to the right by one
+    @inbounds for i in M_out:-1:2
+        u[i] = u[i-1]
+    end
+    u[1] = 0
+
+    # sample descending sequence of sorted random numbers
+    # Algorithm by:
+    # Bentley & Saxe, ACM Transactions on Mathematical Software, Vol 6, No 3
+    # September 1980, Pages 359--364
+    CurMax = one(eltype(u))
+    idx = zeros(Int, M_out)
+    bindex = M_out # bin index
+    @inbounds for i in M_out:-1:1
+        CurMax *= exp(log(rand()) / i)
         # scale random numbers (this is equivalent to normalizing u)
-        rsample = r[i] * usum
-        # checking bindex <= M_out - 1 is redundant since
-        # u[M_in] = usum
-        while rsample > u[bindex]
-            bindex += 1
+        rsample = CurMax * usum
+        # checking bindex >= 1 is redundant since
+        # ucum[1] = 0
+        while rsample < u[bindex]
+            bindex -= 1
         end
         idx[i] = bindex
     end
-    return nothing
-end
-
-function outer_indices!(u)
-    M_out       = length(u)
-    u          .= cumsum(u)
-    idx         = CuArray{Int}(undef, M_out)
-    r           = CuArray{Float32}(undef, M_out)
-
-    rng = GPUArrays.default_rng(CuArray)
-
-    kernel  = @cuda launch=false kernel_outer_indices!(idx, u, r, rng.state)
-    config  = launch_configuration(kernel.fun)
-    threads = Base.min(M_out, config.threads, 256)
-    blocks  = cld(M_out, threads)
-    kernel(idx, u, r, rng.state; threads=threads, blocks=blocks)
     return idx
 end
 
--- test/resample.jl
+++ test/resample.jl
@@ -13,29 +13,43 @@
         )
 
         u = CUDA.rand(m_out)
+
         println("")
-        println("Benchmarking function outer_resample!: should take about 900μs")
-        display(@benchmark CUDA.@sync outer_resample!($state, $model, $u))
+        println("Benchmarking function outer_resample!: should take about 300μs")
+        display(@benchmark outer_resample!($state, $model, $u))
         println("")
     end
+
     @testset "correctness of values" begin
         using BinomialSynapses: outer_indices!
-        M_out = 4
-        u = cu([1f3, 1f0, 1f0, 1f3])
-        outer_indices!(u)
-        @test u == cu([1f3, 1.001f3, 1.002f3, 2.002f3])
+
+        for M_out in [4, 8, 16, 32, 64, 128, 256, 512, 1024]
+            u = rand(M_out)
+            v = cumsum(u)
+            outer_indices!(u)
+            @test u[2:M_out] ≈ v[1:M_out-1]
+        end
 
         count = 0
         for i in 1:100
             u = cu([1f3, 1f0, 1f0, 1f3])
-            outer_indices!(u)
-            uu = Array(u)
+            idx = Array(outer_indices!(u))
             for j in 1:4
-                if uu[j] == 2 || uu[j] == 3
+                if idx[j] == 2 || idx[j] == 3
                     count += 1
                 end
             end
         end
-        @test count / 400 < 0.01
+        @test count / 400 < 0.05
+    end
+
+    @testset "sortedness of idx" begin
+        using BinomialSynapses: outer_indices!
+
+        for M_out in [4, 8, 16, 32, 64, 128, 256, 512, 1024]
+            u = rand(M_out)
+            idx = outer_indices!(u)
+            @test issorted(idx)
+        end
     end
 end
