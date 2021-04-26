b222e0e39dd349f8c563617423d02adeb853b2f7
Camille Gontier <cam.gontier@gmail.com>
2021-04-26 13:24:43 +0200

    Update simulate.jl

--- src/simulate.jl
+++ src/simulate.jl
@@ -9,7 +9,7 @@ function NestedFilterSimulation(
     N, p, q, σ, τ,
     Nrng, prng, qrng, σrng, τrng,
     m_out, m_in, width;
-    λ = 1.121,
+    λ = 0.121,
     dt = nothing
 )
     hmodel = ScalarBinomialModel(N, p, q, σ, τ)
