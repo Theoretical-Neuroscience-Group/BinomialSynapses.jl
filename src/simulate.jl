ee746d266b6bcb50a47760d76ff9910dcae4658a
Camille Gontier <cam.gontier@gmail.com>
2021-04-29 18:38:38 +0200

    Update simulate.jl

--- src/simulate.jl
+++ src/simulate.jl
@@ -34,7 +34,7 @@ end
 #    return obs
 #end
 
-function run!(sim::NestedFilterSimulation; T::Int, plot_each_timestep = false)
+function run!(sim::NestedFilterSimulation; T::Int, plot_each_timestep = false, OED = false)
     times = zeros(0)
     epsps = zeros(0)
     time = 0.
@@ -42,6 +42,10 @@ function run!(sim::NestedFilterSimulation; T::Int, plot_each_timestep = false)
     for i in 1:T
         @time begin
             obs = propagate!(sim)
+        end
+        
+        if i < T && OED == true
+            delta = OED(sim, LinRange(0.05,1,25), times, i)
         end
 
         push!(times, time += obs.dt)
@@ -53,65 +57,94 @@ function run!(sim::NestedFilterSimulation; T::Int, plot_each_timestep = false)
     return times, epsps
 end
 
-function OED()
-end
-
-function run_OED!(sim::NestedFilterSimulation; T::Int, plot_each_timestep = false)
-    times = zeros(0)
-    epsps = zeros(0)
-    time = 0.
-    delta = 0.
+function OED(sim::NestedFilterSimulation, deltat_candidates, times, i)
     
-    for i in 1:T
-        
-        obs = propagate_OED!(sim,delta)
-
-        push!(times, time += obs.dt)
-        push!(epsps, obs.EPSP)        
-        
-        if i < T
-                
-            map = MAP(sim)
-            N_star = map[:N]
-            p_star = map[:p]
-            q_star = map[:q]
-            sigma_star = map[:σ]
-            tau_star = map[:τ]
-            
-
-        
-            delta_candidates = LinRange(0.05,1,25)
-
-            x = 1
-            for ii in 1:i
-                x = 1-(1-(1-p_star)*x)*exp(-times[ii]/tau_star)
-            end
-            e_temp = zeros(25)
-            for kk in 1:25
-                x_temp = 1-(1-(1-p_star)*x)*exp(-delta_candidates[kk]/tau_star)
-                e_temp[kk] = x_temp*N_star*p_star*q_star
-            end
-         
-
-            h = zeros(25)
-            for kk in 1:25
-                sim_local = deepcopy(sim)
-                obs = BinomialObservation(e_temp[kk], delta_candidates[kk])
-                update!(sim_local.fstate, obs, sim_local.filter)
-                v = variance(sim_local)
-                h[kk] = v[:τ]
-            end
-            
-            delta = delta_candidates[argmin(h)]   
-            #delta = rand(Exponential(0.121))
-            print(delta)
-            print("\n")
-
-        end       
+    map = MAP(sim)
+    N_star = map[:N]
+    p_star = map[:p]
+    q_star = map[:q]
+    sigma_star = map[:σ]
+    tau_star = map[:τ]
+    
+    x = 1
+    for ii in 1:i
+        x = 1-(1-(1-p_star)*x)*exp(-times[ii]/tau_star)
     end
-
-    return times, epsps
+    e_temp = zeros(length(deltat_candidates))
+    for kk in 1:length(e_temp)
+        x_temp = 1-(1-(1-p_star)*x)*exp(-delta_candidates[kk]/tau_star)
+        e_temp[kk] = x_temp*N_star*p_star*q_star
+    end
+    
+    h = zeros(length(e_temp))
+    for kk in 1:length(e_temp)
+        sim_local = deepcopy(sim)
+        obs = BinomialObservation(e_temp[kk], delta_candidates[kk])
+        update!(sim_local.fstate, obs, sim_local.filter)
+        v = variance(sim_local)
+        h[kk] = v[:τ]
+    end
+    
+    return delta_candidates[argmin(h)] 
+    
 end
+
+#function run_OED!(sim::NestedFilterSimulation; T::Int, plot_each_timestep = false)
+#    times = zeros(0)
+#    epsps = zeros(0)
+#    time = 0.
+#    delta = 0.
+#    
+#    for i in 1:T
+#        
+#        obs = propagate_OED!(sim,delta)
+#
+#        push!(times, time += obs.dt)
+#        push!(epsps, obs.EPSP)        
+#        
+#        if i < T
+#                
+#            map = MAP(sim)
+#            N_star = map[:N]
+#            p_star = map[:p]
+#            q_star = map[:q]
+#            sigma_star = map[:σ]
+#            tau_star = map[:τ]
+#            
+#
+#        
+#            delta_candidates = LinRange(0.05,1,25)
+#
+#            x = 1
+#            for ii in 1:i
+#                x = 1-(1-(1-p_star)*x)*exp(-times[ii]/tau_star)
+#            end
+#            e_temp = zeros(25)
+#            for kk in 1:25
+#                x_temp = 1-(1-(1-p_star)*x)*exp(-delta_candidates[kk]/tau_star)
+#                e_temp[kk] = x_temp*N_star*p_star*q_star
+#            end
+#         
+#
+#            h = zeros(25)
+#            for kk in 1:25
+#                sim_local = deepcopy(sim)
+#                obs = BinomialObservation(e_temp[kk], delta_candidates[kk])
+#                update!(sim_local.fstate, obs, sim_local.filter)
+#                v = variance(sim_local)
+#                h[kk] = v[:τ]
+#            end
+#            
+#            delta = delta_candidates[argmin(h)]   
+#            #delta = rand(Exponential(0.121))
+#            print(delta)
+#            print("\n")
+#
+#        end       
+#    end
+#
+#    return times, epsps
+#end
 
 MAP(sim::NestedFilterSimulation) = MAP(sim.fstate.model)
 variance(sim::NestedFilterSimulation) = variance(sim.fstate.model)
