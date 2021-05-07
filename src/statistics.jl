ed4c42faf5d343647963d48e76f65f43ba88889d
Simone Carlo Surace <simsurace@gmx.net>
2021-05-07 11:33:38 +0200

    fix indentations

--- src/statistics.jl
+++ src/statistics.jl
@@ -3,46 +3,46 @@ variance(fstate::NestedParticleState) = variance(fstate.model)
 ent(fstate::NestedParticleState) = ent(fstate.model)
 
 function MAP(model::BinomialModel)
-        Dict(
-                :N => mode(Array(model.N)),
-                :p => mode(Array(model.p)),
-                :q => mode(Array(model.q)),
-                :σ => mode(Array(model.σ)),
-                :τ => mode(Array(model.τ))
-        )
+    Dict(
+        :N => mode(Array(model.N)),
+        :p => mode(Array(model.p)),
+        :q => mode(Array(model.q)),
+        :σ => mode(Array(model.σ)),
+        :τ => mode(Array(model.τ))
+    )
 end
 
 function variance(model::BinomialModel)
-        Dict(
-                :N => var(Array(model.N)),
-                :p => var(Array(model.p)),
-                :q => var(Array(model.q)),
-                :σ => var(Array(model.σ)),
-                :τ => var(Array(model.τ))
-        )
+    Dict(
+        :N => var(Array(model.N)),
+        :p => var(Array(model.p)),
+        :q => var(Array(model.q)),
+        :σ => var(Array(model.σ)),
+        :τ => var(Array(model.τ))
+    )
 end
 
 function ent(model::BinomialModel)
-        Dict(
-                :N => entropy(Array(model.N)),
-                :p => entropy(Array(model.p)),
-                :q => entropy(Array(model.q)),
-                :σ => entropy(Array(model.σ)),
-                :τ => entropy(Array(model.τ))
-        )
+    Dict(
+        :N => entropy(Array(model.N)),
+        :p => entropy(Array(model.p)),
+        :q => entropy(Array(model.q)),
+        :σ => entropy(Array(model.σ)),
+        :τ => entropy(Array(model.τ))
+    )
 end
 
 function MAP(model::BinomialGridModel)
-        refresh!(model)
-        MAP(BinomialModel(model.N, model.p, model.q, model.σ, model.τ))
+    refresh!(model)
+    MAP(BinomialModel(model.N, model.p, model.q, model.σ, model.τ))
 end
 
 function variance(model::BinomialGridModel)
-        refresh!(model)
-        variance(BinomialModel(model.N, model.p, model.q, model.σ, model.τ))
+    refresh!(model)
+    variance(BinomialModel(model.N, model.p, model.q, model.σ, model.τ))
 end
 
 function ent(model::BinomialGridModel)
-        refresh!(model)
-        ent(BinomialModel(model.N, model.p, model.q, model.σ, model.τ))
+    refresh!(model)
+    ent(BinomialModel(model.N, model.p, model.q, model.σ, model.τ))
 end
