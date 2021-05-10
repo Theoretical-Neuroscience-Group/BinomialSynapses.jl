c048be507a465eb82e800eeb908dfec269a234fd
Camille Gontier <cam.gontier@gmail.com>
2021-05-10 21:35:03 +0200

    Update statistics.jl

--- src/statistics.jl
+++ src/statistics.jl
@@ -2,13 +2,29 @@ MAP(fstate::NestedParticleState) = MAP(fstate.model)
 variance(fstate::NestedParticleState) = variance(fstate.model)
 ent(fstate::NestedParticleState) = ent(fstate.model)
 
-function MAP(model::BinomialModel)
-    Dict(
-        :N => mode(Array(model.N)),
-        :p => mode(Array(model.p)),
-        :q => mode(Array(model.q)),
-        :σ => mode(Array(model.σ)),
-        :τ => mode(Array(model.τ))
+function MAP(model::BinomialModel; marginal::Bool = false)
+    if marginal
+        return Dict(
+            :N => mode(Array(model.N)),
+            :p => mode(Array(model.p)),
+            :q => mode(Array(model.q)),
+            :σ => mode(Array(model.σ)),
+            :τ => mode(Array(model.τ))
+        )
+    end
+    v = hcat(
+            model.N, 
+            model.p, 
+            model.q, 
+            model.σ, 
+            model.τ
+        ) |> Array |> eachrow |> mode
+    return Dict(
+        :N => v[1],
+        :p => v[2],
+        :q => v[3],
+        :σ => v[4],
+        :τ => v[5]
     )
 end
 
