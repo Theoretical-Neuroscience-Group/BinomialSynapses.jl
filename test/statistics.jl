ef9eb2e01798f34f22d096b2647646875e05847a
Simone Carlo Surace <simsurace@gmx.net>
2021-05-10 18:51:54 +0200

    use joint distribution for MAP

--- test/statistics.jl
+++ test/statistics.jl
@@ -1,6 +1,6 @@
 @testset "statistics.jl" begin
     println("             > statistics.jl")
-    @testset "MAP" begin
+    @testset "MAP, marginal = true" begin
         model = BinomialModel(
                     [1, 2, 2, 2, 4, 4],
                     [0.1, 0.3, 0.4, 0.5, 0.5, 0.5],
@@ -9,12 +9,29 @@
                     [0.1, 0.1, 0.2, 0.3, 0.5, 0.5]
         )
 
-        map = MAP(model)
+        map = MAP(model, marginal = true)
 
         @test map[:N] == 2
         @test map[:p] == 0.5
         @test map[:q] == 0.3
         @test map[:σ] == 0.1
         @test map[:τ] == 0.1
+    end
+    @testset "MAP" begin
+        model = BinomialModel(
+                    [1, 2, 2, 2, 4, 4],
+                    [0.1, 0.3, 0.3, 0.3, 0.5, 0.5],
+                    [0.3, 0.2, 0.2, 0.2, 0.1, 0.3],
+                    [0.1, 0.5, 0.5, 0.5, 0.5, 0.6],
+                    [0.1, 0.8, 0.8, 0.8, 0.5, 0.5]
+        )
+
+        map = MAP(model)
+
+        @test map[:N] == 2
+        @test map[:p] == 0.3
+        @test map[:q] == 0.2
+        @test map[:σ] == 0.5
+        @test map[:τ] == 0.8
     end
 end
