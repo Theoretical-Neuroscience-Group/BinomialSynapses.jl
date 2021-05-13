0014fd6351bb4357d112ce398eb7472a839bb837
Camille Gontier <cam.gontier@gmail.com>
2021-05-13 12:02:03 +0200

    Update emission.jl

--- src/emission.jl
+++ src/emission.jl
@@ -8,7 +8,13 @@ function propagate_emit!(state::BinomialState{<:Array}, model::AbstractBinomialM
         else
             δ = dt
         end
+        print("n =")
+        print(state.n)
+        print("\n")
         propagate!(state, model, δ)
+        print("n =")
+        print(state.n)
+        print("\n") 
         q = model.q[1]
         σ = model.σ[1]
         k = state.k[1,1]
