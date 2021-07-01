fbda53fa661259e928e58910c6ae4cb1ec100bb8
Simone Carlo Surace <simsurace@gmx.net>
2021-07-01 18:19:24 +0200

    add initial tau entropy test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,16 +8,18 @@ using Test
 const RUN_BENCHMARKS = false # optional intermediate benchmarks
 
 @testset "BinomialSynapses.jl" begin
-    include("models.jl")
-    include("propagate.jl")
-    include("timestep.jl")
-    include("emission.jl")
-    include("likelihood.jl")
-    include("jitter.jl")
-    include("resample.jl")
-    include("filter.jl")
-    include("statistics.jl")
-    include("simulate.jl")
+    #include("models.jl")
+    #include("propagate.jl")
+    #include("timestep.jl")
+    #include("emission.jl")
+    #include("likelihood.jl")
+    #include("jitter.jl")
+    #include("resample.jl")
+    #include("filter.jl")
+    #include("statistics.jl")
+    #include("simulate.jl")
 
-    include("_integration.jl")
+    #include("_integration.jl")
+
+    include("_debug.jl")
 end
