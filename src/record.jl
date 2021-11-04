d76fc6730c71ed71cfc3f2a87d4a4b98ffeda248
Camille Gontier <cam.gontier@gmail.com>
2021-11-04 17:14:49 +0100

    Create record.jl
    Linked to comment https://github.com/Theoretical-Neuroscience-Group/BinomialSynapses.jl/pull/37#discussion_r742942189

---
+++ src/record.jl
@@ -0,0 +1,20 @@
+struct Recording{T1, T2, T3}
+    f1::T1
+    f2::T2
+    data::T3
+end
+
+function Recording(f1, f2, sim::NestedFilterSimulation)
+    res = f1(sim)
+    data = [res]
+    return Recording(f1, f2, data)
+end
+
+function update!(rec::Recording, sim)
+    push!(rec.data, rec.f1(sim))
+    return rec
+end
+
+function save(rec::Recording)
+    rec.f2(rec.data)
+end
