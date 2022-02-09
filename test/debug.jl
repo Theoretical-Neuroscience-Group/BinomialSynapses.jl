27db646767c037c17694d28ac023723b40287d47
Simone Carlo Surace <simone.surace@artificialy.com>
2022-02-09 20:17:39 +0100

    Fix test

--- test/debug.jl
+++ test/debug.jl
@@ -1,8 +1,17 @@
 @testset "debug convergence speed" begin
     function run_until_entropy_below!(
-        sim::NestedFilterSimulation; 
         threshold::Real
     )
+        sim = NestedFilterSimulation(
+            10, 0.85, 1.0, 0.2, 0.2,
+            1:20,
+            LinRange(0.00, 1.00, 45),
+            LinRange(0.00, 2.00, 45),
+            LinRange(0.05, 2.00, 45),
+            LinRange(0.05, 2.00, 45),
+            2048, 512, 12,
+            timestep = RandomTimestep(Exponential(0.121))                 
+        )
         if length(sim.times) == 0
             initialize!(sim)
         end
@@ -13,24 +22,20 @@
                 propagate!(sim)
             end
             e = sim.fstate.model.τ |> collect |> proportionmap |> values |> entropy
-            @show e
+            # @show e
             if e < threshold || t > 10000
                 return t
             end
         end
     end
 
-    sim = NestedFilterSimulation(
-        10, 0.85, 1.0, 0.2, 0.2,
-        1:20,
-        LinRange(0.00, 1.00, 45),
-        LinRange(0.00, 2.00, 45),
-        LinRange(0.05, 2.00, 45),
-        LinRange(0.05, 2.00, 45),
-        2048, 512, 12,
-        timestep = RandomTimestep(Exponential(0.121))                 
-    )
-    
-    number_of_timesteps = run_until_entropy_below!(sim, threshold = 1.9)
+    number_of_timesteps = 0.
+    N = 200
+    for i in 1:N
+        nn = run_until_entropy_below!(1.9)
+        @show nn
+        number_of_timesteps += nn
+    end
+    number_of_timesteps /= N
     @show number_of_timesteps
 end
