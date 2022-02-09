62bca7280600dd7c4520fd237cbfe9eb7ea99cdb
simsurace <simsurace@gmx.net>
2022-02-09 20:33:39 +0100

    Add convergence debug test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,16 @@ using Test
 const RUN_BENCHMARKS = false # optional intermediate benchmarks
 
 @testset "BinomialSynapses.jl" begin
-    include("models.jl")
-    include("propagate.jl")
-    include("emission.jl")
-    include("likelihood.jl")
-    include("jitter.jl")
-    include("resample.jl")
-    include("filter.jl")
-    include("statistics.jl")
+    # include("models.jl")
+    # include("propagate.jl")
+    # include("emission.jl")
+    # include("likelihood.jl")
+    # include("jitter.jl")
+    # include("resample.jl")
+    # include("filter.jl")
+    # include("statistics.jl")
 
-    include("_integration.jl")
+    # include("_integration.jl")
+
+    include("debug.jl")
 end
