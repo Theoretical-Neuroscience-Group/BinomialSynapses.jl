d3841b115f7db469e70164fd1cb264b43c476cb5
Camille Gontier <cam.gontier@gmail.com>
2022-04-01 13:25:38 +0200

    Debug

--- src/BinomialSynapses.jl
+++ src/BinomialSynapses.jl
@@ -49,7 +49,7 @@ include("record.jl")
 export Recording
 
 include("simulate.jl")
-export NestedFilterSimulation, initialize!, m_out, run!, runBatch_map!, runBatchTau!
+export NestedFilterSimulation, initialize!, m_out, run!, runBatch_map!, runBatchTau!, NestedFilterExperiment, run_experiment!
 
 include("visualize.jl")
 export posterior_plot
