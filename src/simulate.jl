ae6f9e7409bda064840b96e13dc873ada9de1200
Camille Gontier <cam.gontier@gmail.com>
2022-04-01 17:12:13 +0200

    Update simulate.jl

--- src/simulate.jl
+++ src/simulate.jl
@@ -337,24 +337,7 @@ function Recording(f1, f2, sim::NestedFilterSimulation)
     return Recording(f1, f2, data)
 end
 		
-struct NestedFilterExperiment{T1, T2, T3}
-    filter::T1
-    fstate::T2
-    tsteps::T3
-end
 
-function NestedFilterExperiment(
-    Nrng, prng, qrng, σrng, τrng,
-    m_out, m_in, width;
-    timestep
-)
-    filter = NestedParticleFilter(width)
-    fstate = NestedParticleState(
-                m_out, m_in,
-                Nrng, prng, qrng, σrng, τrng
-             )
-    return NestedFilterExperiment(filter, fstate, timestep)
-end
 		
 function propagate!(sim::NestedFilterExperiment, epsc, dt)
     obs = BinomialObservation(epsc, dt)
