fd3c6e13eb703784e4dfb09aa53f4be642fe7a5f
Camille Gontier <cam.gontier@gmail.com>
2022-04-01 17:12:24 +0200

    Update experiment.jl

--- src/experiment.jl
+++ src/experiment.jl
@@ -1,1 +1,18 @@
+struct NestedFilterExperiment{T1, T2, T3}
+    filter::T1
+    fstate::T2
+    tsteps::T3
+end
 
+function NestedFilterExperiment(
+    Nrng, prng, qrng, σrng, τrng,
+    m_out, m_in, width;
+    timestep
+)
+    filter = NestedParticleFilter(width)
+    fstate = NestedParticleState(
+                m_out, m_in,
+                Nrng, prng, qrng, σrng, τrng
+             )
+    return NestedFilterExperiment(filter, fstate, timestep)
+end
