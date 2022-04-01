b8425db8b8ba15c35f552286d1f6b889216685a5
Camille Gontier <cam.gontier@gmail.com>
2022-04-01 15:30:26 +0200

    Add experiment

--- src/BinomialSynapses.jl
+++ src/BinomialSynapses.jl
@@ -60,6 +60,7 @@ export OEDPolicy, policy, Uniform
 include("myopic.jl")
 export MyopicPolicy, Myopic, MyopicFast, Myopic_tau, MyopicFast_tau
 
-
+include("experiment.jl")
+export NestedFilterExperiment, run_experiment!
 
 end#module
