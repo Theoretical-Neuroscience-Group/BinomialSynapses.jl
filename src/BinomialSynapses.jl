cefdd1763c8cbdfbf8d033a1b24a1fdefa58b618
Camille Gontier <cam.gontier@gmail.com>
2022-05-01 15:47:56 +0200

    Add run_exact !

--- src/BinomialSynapses.jl
+++ src/BinomialSynapses.jl
@@ -52,7 +52,7 @@ include("record.jl")
 export Recording
 
 include("simulate.jl")
-export NestedFilterSimulation, initialize!, m_out, run!, runBatch_map!, runBatchTau!
+export NestedFilterSimulation, initialize!, m_out, run!, runBatch_map!, runBatchTau!, run_exact!
 
 include("visualize.jl")
 export posterior_plot
