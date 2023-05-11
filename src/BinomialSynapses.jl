8a741c78b7ac747df7989eeacf67b2b051e416d2
Camille Gontier <cam.gontier@gmail.com>
2023-05-11 12:31:44 -0400

    Add estimate_posterior!

--- src/BinomialSynapses.jl
+++ src/BinomialSynapses.jl
@@ -52,7 +52,7 @@ include("record.jl")
 export Recording
 
 include("simulate.jl")
-export NestedFilterSimulation, initialize!, m_out, run!, runBatch_map!, runBatchTau!, run_exact_1!, run_exact_2!
+export NestedFilterSimulation, initialize!, m_out, run!, runBatch_map!, runBatchTau!, run_exact_1!, run_exact_2!, estimate_posterior!
 
 include("visualize.jl")
 export posterior_plot
