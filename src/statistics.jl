717141b804db9036529f44768db038900393f0e0
Camille Gontier <cam.gontier@gmail.com>
2023-05-29 18:07:24 -0400

    debug

--- src/statistics.jl
+++ src/statistics.jl
@@ -32,7 +32,7 @@ function MAP(model::BinomialModel{T1, T2}; marginal::Bool = false) where {T1, T2
             model.σ, 
             model.τ
         ) |> Array |> eachrow |> mode
-    print(countmap(collect(eachrow(v))))
+    print(StatsBase.countmap(collect(eachrow(v))))
     print("\n")
     return BinomialModel{eltype(T1), eltype(T2)}(eltype(T1)(v[1]), v[2:end]...)
 end
